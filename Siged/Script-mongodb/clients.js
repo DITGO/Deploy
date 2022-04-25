4467804ee2e28671a3060800cbe6a049c99d2088
DITGO <88164111+DITGO@users.noreply.github.com>
2022-04-25 14:50:10 -0300

    Add files via upload

--- Siged/Script-mongodb/clients.js
+++ Siged/Script-mongodb/clients.js
@@ -4,7 +4,6 @@ db.clients.drop()
 
 db.clients.insert(
 [
-{ "userID": "6182bd65871bb40056e97348", "name": "ABADIO SOUZA E SILVA", "cpf": "54885957168", "email": "ABADIO.SILVA@POLICIACIVIL.GO.GOV.BR", "phone": "62984953422", "secondaryPhone": "06232012652", "address": "Não Informado", "office": "DELEGADO DE POLÍCIA CLASSE ESPECIAL", "location": "6182cd3b555069694281e393", "address": "Não Informado", "history": [{ "userID": "6182bd65871bb40056e97348", "date": "2021-11-03T13:57:39.000z", "label": "created" }], "active": true, "features": "6182cd3a5827d211cd5aedf2", "image": "" },
-{ "userID": "6182bd65871bb40056e97348", "name": "ABIMAR LEONEL GOMES", "cpf": "47668075100", "email": "ABIMARLG@POLICIACIVIL.GO.GOV.BR", "phone": "06199378657", "secondaryPhone": "06136371145", "address": "Não Informado", "office": "AGENTE AUXILIAR POLICIAL", "location": "6182cd3b555069694281e2c6", "address": "Não Informado", "history": [{ "userID": "6182bd65871bb40056e97348", "date": "2021-11-03T13:57:39.000z", "label": "created" }], "active": true, "features": "6182cd3a5827d211cd5aedf2", "image": "" },
+{ "userID": "625ec190bbf25e0054e3f1c9", "name": "ABADIO SOUZA E SILVA", "cpf": "54885957168", "email": "ABADIO.SILVA@POLICIACIVIL.GO.GOV.BR", "phone": "62984953422", "secondaryPhone": "06232012652", "office": "DELEGADO DE POLÍCIA CLASSE ESPECIAL", "location": "625ecbab2dab701016bd7849", "address": "Não Informado", "history": [{ "userID": "625ec190bbf25e0054e3f1c9", "date": "2021-11-03T13:57:39.000z", "label": "created" }], "active": true, "features": "625ecbab25baeba1cacc0824", "image": "" }
 ]
 )
