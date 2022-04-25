52f6f1b6bdd6751d04082872826c68cfc8a47e15
DITGO <88164111+DITGO@users.noreply.github.com>
2022-04-25 15:07:07 -0300

    Add files via upload

---
+++ Siged/Script-mongodb/clients.js
@@ -0,0 +1,10 @@
+use clients_database
+
+db.clients.drop()
+
+db.clients.insert(
+[
+
+{ "userID": "625ec190bbf25e0054e3f1c9", "name": "ABADIO SOUZA E SILVA", "cpf": "54885957168", "email": "abadio.silva@policiacivil.go.gov.br", "phone": "62984953422", "secondaryPhone": "06232012652", "office": "DELEGADO DE POLÍCIA CLASSE ESPECIAL", "location": "625ecbab2dab701016bd7849", "address": "Não Informado", "history": [{ "userID": "625ec190bbf25e0054e3f1c9", "date": "2021-11-03T13:57:39.000z", "label": "created" }], "active": true, "features": "625ecbab25baeba1cacc0824", "image": "", "gender": "" }
+]
+)
