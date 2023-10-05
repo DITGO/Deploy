8a2d12d04f925fd0e9c5601773e8bfc02dacd01a
DITGO <88164111+DITGO@users.noreply.github.com>
2023-10-05 17:49:45 -0300

    Update workstation-regionais.js

--- Schedula/workstation-regionais.js
+++ Schedula/workstation-regionais.js
@@ -1,3 +1,4 @@
 \c schedula_location
 
-INSERT INTO workstation (id, name, "cityId", phone, ip, "parentWorkstationId", gateway, is_regional, vpn) VALUES (uuid_generate_v4(), 'SPJ SUPERINTENDÊNCIA DE POLÍCIA JUDICIÁRIA', 'c21b95cb-018f-4a80-bc54-022e72cd7453', '', '0.0.0.0 ~ 0.0.0.0', '9cd53c05-48f1-4e35-870b-a512b2c4a2dd', '', false, false);
+INSERT INTO workstation (id, name, "cityId", phone, ip, gateway, is_regional, vpn) VALUES (uuid_generate_v4(), 'GERÊNCIA DE GESTÃO E FINANÇAS', 'c21b95cb-018f-4a80-bc54-022e72cd7453', '', '0.0.0.0 ~ 0.0.0.0', 'e593eb52-aa03-48b6-9211-40ceb7f9476a', '', false, false);
+INSERT INTO workstation (id, name, "cityId", phone, ip, gateway, is_regional, vpn) VALUES (uuid_generate_v4(), 'GERÊNCIA DE APOIO ADMINISTRATIVO DA POLÍCIA CIVIL', 'c21b95cb-018f-4a80-bc54-022e72cd7453', '', '0.0.0.0 ~ 0.0.0.0', 'e593eb52-aa03-48b6-9211-40ceb7f9476a', '', false, false);
