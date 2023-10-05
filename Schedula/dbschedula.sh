3a59985f16959b18a8f6ae4bd075216c4dc84699
DITGO <88164111+DITGO@users.noreply.github.com>
2023-10-05 18:08:35 -0300

    Update dbschedula.sh

--- Schedula/dbschedula.sh
+++ Schedula/dbschedula.sh
@@ -2,9 +2,9 @@
 
 #docker cp city.js schedula_location_db:/home
 #docker cp workstation-regionais.js schedula_location_db:/home
-#docker cp workstation.js schedula_location_db:/home
+docker cp workstation.js schedula_location_db:/home
 
 #docker exec -u 0 -i schedula_location_db /bin/bash < city.sh
 #docker exec -u 0 -i schedula_location_db /bin/bash < workstation-regionais.sh
-#docker exec -u 0 -i schedula_location_db /bin/bash < workstation.sh
+docker exec -u 0 -i schedula_location_db /bin/bash < workstation.sh
 
