7478c2b9f427b33aab979441d2ce59db410847d9
DITGO <88164111+DITGO@users.noreply.github.com>
2023-10-05 17:41:40 -0300

    Update workstation-regionais.js

--- Schedula/workstation-regionais.js
+++ Schedula/workstation-regionais.js
@@ -1,3 +1,3 @@
 \c schedula_location
 
-INSERT INTO workstation (id, name, "cityId", phone, ip, gateway, is_regional, vpn) VALUES (uuid_generate_v4(), 'DIRETORIA GERAL DA POLÍCIA CIVIL', 'c21b95cb-018f-4a80-bc54-022e72cd7453', '', '0.0.0.0 ~ 0.0.0.0', '', false, false);
+INSERT INTO workstation (id, name, "cityId", phone, ip, "parentWorkstationId", gateway, is_regional, vpn) VALUES (uuid_generate_v4(), 'GABINETE DO DELEGADO GERAL DA POLÍCIA CIVIL', 'c21b95cb-018f-4a80-bc54-022e72cd7453', '', '10.242.33.129 ~ 10.242.33.254', 'b69be7c7-30ed-4fce-a7e4-1b182fa38805', '10.242.33.1', false, false);
