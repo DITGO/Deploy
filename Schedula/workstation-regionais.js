aef177d16cdae07d3610543aa4b14f70ff768a73
DITGO <88164111+DITGO@users.noreply.github.com>
2023-10-05 17:51:00 -0300

    Update workstation-regionais.js

--- Schedula/workstation-regionais.js
+++ Schedula/workstation-regionais.js
@@ -1,4 +1,4 @@
 \c schedula_location
 
-INSERT INTO workstation (id, name, "cityId", phone, ip, gateway, is_regional, vpn) VALUES (uuid_generate_v4(), 'GERÊNCIA DE GESTÃO E FINANÇAS', 'c21b95cb-018f-4a80-bc54-022e72cd7453', '', '0.0.0.0 ~ 0.0.0.0', 'e593eb52-aa03-48b6-9211-40ceb7f9476a', '', false, false);
-INSERT INTO workstation (id, name, "cityId", phone, ip, gateway, is_regional, vpn) VALUES (uuid_generate_v4(), 'GERÊNCIA DE APOIO ADMINISTRATIVO DA POLÍCIA CIVIL', 'c21b95cb-018f-4a80-bc54-022e72cd7453', '', '0.0.0.0 ~ 0.0.0.0', 'e593eb52-aa03-48b6-9211-40ceb7f9476a', '', false, false);
+INSERT INTO workstation (id, name, "cityId", phone, ip, "parentWorkstationId", gateway, is_regional, vpn) VALUES (uuid_generate_v4(), 'GERÊNCIA DE GESTÃO E FINANÇAS', 'c21b95cb-018f-4a80-bc54-022e72cd7453', '', '0.0.0.0 ~ 0.0.0.0', 'e593eb52-aa03-48b6-9211-40ceb7f9476a', '', false, false);
+INSERT INTO workstation (id, name, "cityId", phone, ip, "parentWorkstationId", gateway, is_regional, vpn) VALUES (uuid_generate_v4(), 'GERÊNCIA DE APOIO ADMINISTRATIVO DA POLÍCIA CIVIL', 'c21b95cb-018f-4a80-bc54-022e72cd7453', '', '0.0.0.0 ~ 0.0.0.0', 'e593eb52-aa03-48b6-9211-40ceb7f9476a', '', false, false);
